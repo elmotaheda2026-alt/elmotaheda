6c297fad8a90c85eeb67d760d588e8afab652a25
mohamed talat <mohamedtalat@gmail.com>
2026-06-29 05:06:11 +0300

    bbbb

--- al-muttahida-saas/src/pages/Customers.tsx
+++ al-muttahida-saas/src/pages/Customers.tsx
@@ -782,25 +782,6 @@ export default function Customers() {
           </div>
         </div>
       )}
-    </div>
-  );
-}e-none text-sm"
-                        />
-                        <input
-                          type="text"
-                          placeholder="صلة القرابة"
-                          value={formData.guarantors[2]?.relationship || ''}
-                          onChange={(e) => updateGuarantor(2, 'relationship', e.target.value)}
-                          className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none text-sm"
-                        />
-                      </div>
-                    </div>
-                  </div>
-              </div>
-            </div>
-          </form>
-        </div>
-      )}
 
       {/* Search Modal */}
       {searchModal && (
